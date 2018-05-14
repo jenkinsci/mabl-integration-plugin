bde73d9caa525f64dea669203e493dcfbd9b7331
Mike DeCosta <mablloaner@mabls-MacBook.local>
2018-05-14 18:02:43 -0400

    Fixes to some formatting

--- src/main/java/com/mabl/integration/jenkins/validation/MablRestApiClientRetryHandler.java
+++ src/main/java/com/mabl/integration/jenkins/validation/MablRestApiClientRetryHandler.java
@@ -11,16 +11,20 @@ import static org.apache.commons.httpclient.HttpStatus.SC_NOT_IMPLEMENTED; // 50
 import static org.apache.commons.httpclient.HttpStatus.SC_BAD_GATEWAY; // 502
 
 public class MablRestApiClientRetryHandler extends DefaultServiceUnavailableRetryStrategy {
-    private final ArrayList<Integer> retryStatusCodes = new ArrayList<Integer>(Arrays.asList(SC_NOT_IMPLEMENTED, SC_BAD_GATEWAY));
-    private final int maxExecutionCount = 5;
+    private final ArrayList<Integer> retryStatusCodes = new ArrayList<Integer>(Arrays.asList(
+            SC_NOT_IMPLEMENTED,
+            SC_BAD_GATEWAY
+    ));
+    private final int maxRetries = 5;
 
     @Override
     public boolean retryRequest(HttpResponse response, int executionCount, HttpContext context) {
-        return isRetryStatusCode(response.getStatusLine().getStatusCode()) && executionCount < maxExecutionCount;
+        return isRetryStatusCode(response.getStatusLine().getStatusCode())
+                && executionCount <= this.maxRetries;
     }
 
     private boolean isRetryStatusCode(int statusCode) {
-        return retryStatusCodes.contains(statusCode);
+        return this.retryStatusCodes.contains(statusCode);
     }
 
 }
